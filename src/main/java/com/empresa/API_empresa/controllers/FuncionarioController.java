4421a277a9b0be6a1fd30b846a079378e4131ef3
Tiago Souza <tiagoadesouza21@gmail.com>
2025-04-15 09:53:57 -0300

    Metodo post pega dos dados do Json e cria uma entidade funcionario com esses dados

---
+++ src/main/java/com/empresa/API_empresa/controllers/FuncionarioController.java
@@ -0,0 +1,26 @@
+package com.empresa.API_empresa.controllers;
+
+import com.empresa.API_empresa.endereco.Endereco;
+import com.empresa.API_empresa.funcionario.entities.DadosCadastroFuncionario;
+import com.empresa.API_empresa.funcionario.entities.Funcionario;
+import com.empresa.API_empresa.funcionario.entities.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("funcionarios")
+public class FuncionarioController {
+
+    //injeção dependencia
+    @Autowired
+    private FuncionarioRepository repository;
+
+    @PostMapping
+    public void cadastrarFuncionario(@RequestBody DadosCadastroFuncionario dados){
+        repository.save(new Funcionario(dados));
+    }
+
+}
