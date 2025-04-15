062eb1d08abcc759b5b6426869c64d09548298fc
Tiago Souza <tiagoadesouza21@gmail.com>
2025-04-15 12:36:40 -0300

    Adicionando @Transactional para transações no Post

--- src/main/java/com/empresa/API_empresa/controllers/FuncionarioController.java
+++ src/main/java/com/empresa/API_empresa/controllers/FuncionarioController.java
@@ -4,6 +4,7 @@ import com.empresa.API_empresa.endereco.Endereco;
 import com.empresa.API_empresa.funcionario.entities.DadosCadastroFuncionario;
 import com.empresa.API_empresa.funcionario.entities.Funcionario;
 import com.empresa.API_empresa.funcionario.entities.FuncionarioRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +20,7 @@ public class FuncionarioController {
     private FuncionarioRepository repository;
 
     @PostMapping
+    @Transactional
     public void cadastrarFuncionario(@RequestBody DadosCadastroFuncionario dados){
         repository.save(new Funcionario(dados));
     }
