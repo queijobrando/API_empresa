af5d51bf028e6d2a156dab1b571b1b4ec024368b
Tiago Azevedo <tiagoadesouza21@gmail.com>
2025-04-14 21:00:40 -0300

    first commit

---
+++ src/test/java/com/empresa/API_empresa/ApiEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.API_empresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
