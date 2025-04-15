cb6485adfea5bb27626dca4ab67ec5d5cfce3c87
Tiago Souza <tiagoadesouza21@gmail.com>
2025-04-15 12:37:19 -0300

    mudando o nome da table para funcionarios

--- src/main/java/com/empresa/API_empresa/funcionario/entities/Funcionario.java
+++ src/main/java/com/empresa/API_empresa/funcionario/entities/Funcionario.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity(name = "Funcionario")
-@Table(name = "medicos")
+@Table(name = "funcionarios")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
